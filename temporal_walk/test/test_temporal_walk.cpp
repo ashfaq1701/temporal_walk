cab279be834622491b2b4f1934adfff0de8c7d53
ashfaq1701 <ashfaq.aws@gmail.com>
2024-11-28 20:32:17 +0000

    Test cases updated

--- temporal_walk/test/test_temporal_walk.cpp
+++ temporal_walk/test/test_temporal_walk.cpp
@@ -13,7 +13,7 @@ constexpr int64_t MAX_TIME_CAPACITY = 5;
 
 constexpr int RANDOM_START = 0;
 constexpr int RANDOM_END = 10000;
-constexpr int RANDOM_NUM_SAMPLES = 100000;
+constexpr int RANDOM_NUM_SAMPLES = 1000000;
 
 class RandomPickerTest : public ::testing::Test {
 protected:
@@ -234,7 +234,7 @@ TEST_F(RandomPickerTest, TwoElementRangeDistributionTestForExponentialRandomPick
     const double actual_prob_start = static_cast<double>(count_ones_start_prioritized) / num_trials;
 
     // Allow for some statistical variation
-    constexpr double tolerance = 0.02;  // 2% tolerance
+    constexpr double tolerance = 0.01;  // 1% tolerance
 
     EXPECT_NEAR(actual_prob_end, expected_prob_end, tolerance)
         << "When prioritizing end, probability of picking 1 should be approximately "
