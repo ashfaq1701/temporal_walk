4cc4c7c8cca746b5c2d468e61d2e8695eab13ab7
ashfaq1701 <ashfaq.aws@gmail.com>
2024-12-25 19:18:39 +0000

    Updated version settings

--- setup.py
+++ setup.py
@@ -65,6 +65,6 @@ setup(
     ext_modules=[CMakeExtension('_temporal_walk')],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
-    python_requires=">=3.10",
+    python_requires=">=3",
     install_requires=["pybind11>=2.6.0", "numpy", "networkx"],
 )
